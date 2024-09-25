71c467d6c785f1abc895faf98c8b51362272b702
Rukaiya Tahsin <126619366+tahsinrukaiya@users.noreply.github.com>
2024-09-25 22:52:34 +0200

    displaying products

--- starter-code/src/components/ProductCard.jsx
+++ starter-code/src/components/ProductCard.jsx
@@ -1,25 +1,59 @@
+import { useEffect, useState } from 'react';
+const url = 'https://v2.api.noroff.dev/online-shop';
 
-export default function ProductCard({ product }) {
+export default function ProductCard() {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                // Reset the error state in case there as an error previously
+                setIsError(false);
+                // Turn on the loading state each time we do an API call
+                setIsLoading(true);
+                const response = await fetch(url);
+                const json = await response.json();
+                setData(json.data);
+                console.log(json.data);
+                // Clear the loading state once we've successfully got our data
+                setIsLoading(false);
+            } catch (error) {
+                // Clear the loading state if we get an error and then
+                // set our error state to true
+                console.error("Error fetching data:", error);
+                setIsLoading(false);
+                setIsError(true);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading products</div>;
+    }
+
+    if (isError) {
+        return <div>Error loading data</div>;
+    }
     return (
         <>
             <div className="card_container">
-                <div className="product_card">
-                    <div className="product">
-                        <div className="product_image">
-                            <img src={product.imageUrl} alt={product.title} className="product_image" /></div>
-                        <div className="product_title"><h4>{product.title}</h4>
-                            <span>{product.price} kr</span></div>
-                        <div className="stars">
-                            <i className="fa-solid fa-star"></i>
-                        </div>
-                        <div className="button>"><button className="add_button">Add to Cart</button></div>
-
+                {data.map((product) => (
+                    <div className="product_card" key={product.id}>
+                        <img src={product.image.url} alt={product.title} className="product_img" />
+                        <div><h4>{product.title}</h4></div>
+                        <div><p>{product.price}kr</p></div>
                     </div>
-                </div>
+                ))}
             </div>
         </>
-    )
+    );
+
 }
+
 
 
 
--- starter-code/src/components/pages/Home.jsx
+++ starter-code/src/components/pages/Home.jsx
@@ -1,37 +1,12 @@
 import ProductCard from "../ProductCard";
-import { useEffect, useState } from 'react';
-
-const url = 'https://v2.api.noroff.dev/online-shop';
 
 export default function Home() {
-    const [products, setProducts] = useState([]);
-
-    // The useEffect will run once when the component first mounts
-    useEffect(() => {
-        // Function that gets our posts
-        async function getData() {
-            const response = await fetch(url);
-            const json = await response.json();
-            console.log(json);
-            // Setting our `posts` state to the API data we received
-            setProducts(json);
-        }
-        getData();
-    }, []);
 
     return (
         <div>
             <h2 className="heading_one">All Products</h2>
+            <ProductCard />
 
-            <div className="product-grid">
-                {Array.isArray(products) && products.length > 0 ? (
-                    products.map((product) => (
-                        <ProductCard key={product.id} product={product} />
-                    ))
-                ) : (
-                    <p>Loading products...</p>
-                )}
-            </div>
         </div>
     );
 }
